8d4600bcf9725bf68bea0862e71b995bb0c5f477
Justin Beall <jus.beall@gmail.com>
2018-03-01 00:02:53 -0500

    Update build status model

--- app/model/build_details.py
+++ app/model/build_details.py
@@ -1,11 +1,13 @@
 class BuildDetails:
-    def __init__(self, *, branch_name: str, build_number: int,
-                 queue_time: str = None, time: str = None, changes: list = None):
+    def __init__(self, *, branch_name: str, build_number: int, start_time: str = None,
+                 queue_time: str = None, total_time: str = None, changes: list = None):
         self.branch_name = branch_name
         self.build_number = build_number
+        self.start_time = start_time
         self.queue_time = queue_time
-        self.time = time
+        self.total_time = total_time
         self.changes = changes
 
     def __str__(self):
-        return f'{self.branch_name},{self.build_number},{self.queue_time},{self.time},{self.changes}'
+        return f'{self.branch_name},{self.build_number},' \
+               f'{self.start_time},{self.queue_time},{self.total_time},{self.changes}'
--- tests/model/build_details_test.py
+++ tests/model/build_details_test.py
@@ -4,23 +4,26 @@ from app.model.build_details import BuildDetails
 def test_build_details_init():
     expected_branch_name = 'branch_name'
     expected_build_number = 1
+    expected_start_time = 'Feb 28, 2018 3:10:34 PM'
     expected_queue_time = '3 ms'
-    expected_time = '47 m'
+    expected_total_time = '47 m'
     expected_changes = ['changes']
 
-    expected_build_details_str = 'branch_name,1,3 ms,47 m,[\'changes\']'
+    expected_build_details_str = 'branch_name,1,Feb 28, 2018 3:10:34 PM,3 ms,47 m,[\'changes\']'
 
     build_details = BuildDetails(
         branch_name=expected_branch_name,
         build_number=expected_build_number,
+        start_time=expected_start_time,
         queue_time=expected_queue_time,
-        time=expected_time,
+        total_time=expected_total_time,
         changes=['changes']
     )
 
     assert expected_branch_name == build_details.branch_name
     assert expected_build_number == build_details.build_number
+    assert expected_start_time == build_details.start_time
     assert expected_queue_time == build_details.queue_time
-    assert expected_time == build_details.time
+    assert expected_total_time == build_details.total_time
     assert expected_changes == build_details.changes
     assert expected_build_details_str == str(build_details)
