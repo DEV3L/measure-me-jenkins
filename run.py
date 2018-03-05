befc24975cd56018794a4e657a2dd3c7323bec46
Justin Beall <jus.beall@gmail.com>
2018-03-04 21:54:16 -0500

    Update run to print status and details

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
+from logging import getLogger
+
 from selenium.webdriver.chrome.webdriver import WebDriver as ChromeWebDriver
 
+from app.actions.fetch_build_details import FetchBuildDetails
 from app.actions.fetch_build_numbers_for_branch import FetchBuildNumbersForBranch
-from app.actions.fetch_build_output import FetchBuildOutput
+from app.actions.fetch_build_status import FetchBuildStatus
 from app.actions.log_into_github import LogIntoGitHub
 from app.actions.log_into_jenkins import LogIntoJenkins
-from app.builders.build_status_builder import BuildStatusBuilder
 from app.clients.browser_client import BrowserClient
+
+logger = getLogger(__name__)
+branch_name = 'master'
 
 
 def _run(chrome_web_driver: ChromeWebDriver()):
@@ -17,27 +22,33 @@ def _run(chrome_web_driver: ChromeWebDriver()):
     log_into_jenkins = LogIntoJenkins(browser_client)
     log_into_jenkins.execute()
 
-    fetch_build_information_for_branch = FetchBuildNumbersForBranch(browser_client, 'master')
+    fetch_build_information_for_branch = FetchBuildNumbersForBranch(browser_client, branch_name)
     build_numbers = fetch_build_information_for_branch.execute()
 
-    build_statuses = []
+    build_status_list = []
+    build_details_list = []
 
     for build_number in build_numbers:
-        fetch_build_output = FetchBuildOutput(browser_client, 'master', build_number)
-        build_output = fetch_build_output.execute()
+        fetch_build_status = FetchBuildStatus(browser_client, branch_name, build_number)
+        build_status = fetch_build_status.execute()
+        build_status_list.append(build_status)
 
-        build_status_builder = BuildStatusBuilder('master', build_number, build_output)
-        build_statuses.append(build_status_builder.build())
+        fetch_build_details = FetchBuildDetails(browser_client, branch_name, build_number)
+        build_details = fetch_build_details.execute()
+        build_details_list.append(build_details)
 
+    print('branch|build number|status|branch|build number|start time|queue time|total time|changes')
 
-    for build_status in build_statuses:
-        print(build_status)
+    for build_details, build_status in zip(build_details_list, build_status_list):
+        print(f'{build_status}|{build_details}')
+
 
 if __name__ == '__main__':
     chrome_web_driver = ChromeWebDriver()
     try:
         _run(chrome_web_driver)
-    except:
+    except Exception as e:
+        logger.exception(e)
         chrome_web_driver.close()
     else:
         chrome_web_driver.close()
