1ac3ea27f19bdd3711020e61a3cee3bb2af2e3f1
Justin Beall <jus.beall@gmail.com>
2018-03-07 23:31:37 -0500

    Add failures to build details model

--- app/model/build_details.py
+++ app/model/build_details.py
@@ -1,16 +1,17 @@
 class BuildDetails:
     def __init__(self, *, branch_name: str, build_number: int, start_time: str = None,
-                 queue_time: float = None, total_time: float = None, changes: list = None):
+                 queue_time: float = None, total_time: float = None, changes: list = None, failures: list = None):
         self.branch_name = branch_name
         self.build_number = build_number
         self.start_time = start_time
         self.queue_time = queue_time
         self.total_time = total_time
         self.changes = changes
+        self.failures = failures
 
     def __str__(self):
         return f'{self.branch_name}|{self.build_number}|' \
-               f'{self.start_time}|{self.queue_time}|{self.total_time}|{self.changes}'
+               f'{self.start_time}|{self.queue_time}|{self.total_time}|{self.changes}|{self.failures}'
 
     def __eq__(self, other):
         return str(self) == str(other)
--- tests/model/build_details_test.py
+++ tests/model/build_details_test.py
@@ -5,11 +5,12 @@ def test_build_details_init():
     expected_branch_name = 'branch_name'
     expected_build_number = 1
     expected_start_time = 'Feb 28, 2018 3:10:34 PM'
-    expected_queue_time = '3 ms'
-    expected_total_time = '47 m'
+    expected_queue_time = .003
+    expected_total_time = 47
     expected_changes = ['changes']
+    expected_failures = ['failures']
 
-    expected_build_details_str = 'branch_name|1|Feb 28, 2018 3:10:34 PM|3 ms|47 m|[\'changes\']'
+    expected_build_details_str = 'branch_name|1|Feb 28, 2018 3:10:34 PM|0.003|47|[\'changes\']|[\'failures\']'
 
     build_details = BuildDetails(
         branch_name=expected_branch_name,
@@ -17,7 +18,8 @@ def test_build_details_init():
         start_time=expected_start_time,
         queue_time=expected_queue_time,
         total_time=expected_total_time,
-        changes=['changes']
+        changes=['changes'],
+        failures=expected_failures
     )
 
     assert expected_branch_name == build_details.branch_name
@@ -26,4 +28,5 @@ def test_build_details_init():
     assert expected_queue_time == build_details.queue_time
     assert expected_total_time == build_details.total_time
     assert expected_changes == build_details.changes
+    assert expected_failures == build_details.failures
     assert expected_build_details_str == str(build_details)
